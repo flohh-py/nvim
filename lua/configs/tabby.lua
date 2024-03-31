d58a412ac89c4783ae5e0881b64070cb0f641a22
flohh-py <jbmflohh@gmail.com>
2024-03-31 15:57:12 -0400

    update tabby config

--- lua/configs/tabby.lua
+++ lua/configs/tabby.lua
@@ -1,3 +1,3 @@
-vim.g.tabby_trigger_mode = 'auto'
-vim.g.tabby_keybinding_accept = '<S-Right>'
+vim.g.tabby_trigger_mode = 'manual'
+vim.g.tabby_keybinding_accept = '<C-Right>'
 vim.g.tabby_keybinding_trigger_or_dismiss = '<C-\\>'
