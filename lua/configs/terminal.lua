f46922ed03952c6da66641b72b8f98cff4ae94ab
flohh-py <jbmflohh@gmail.com>
2024-04-02 11:17:15 -0400

    config on terminal

--- lua/configs/terminal.lua
+++ lua/configs/terminal.lua
@@ -7,12 +7,10 @@ local lazygit   = Terminal:new({
     float_opts = {
         border = "double",
     },
-    -- function to run on opening the terminal
     on_open = function(term)
         vim.cmd("startinsert!")
         vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
     end,
-    -- function to run on closing the terminal
     on_close = function(term)
         vim.cmd("startinsert!")
     end,
@@ -22,8 +20,13 @@ local termfloat = Terminal:new({
     direction = "float",
     on_open = function(term)
         vim.cmd("startinsert!")
-        vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<esc>", "<cmd>lua TermfloatToggle()<CR>",
-            { noremap = true, silent = true })
+        vim.api.nvim_buf_set_keymap(
+            term.bufnr,
+            "t",
+            "<F1>",
+            "<cmd>close<CR>",
+            { noremap = true, silent = true }
+        )
     end,
 })
 
