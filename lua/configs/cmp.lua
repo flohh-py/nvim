e569697192b17f1f79907b36a5b4ea4e36d52d65
flohh-py <jbmflohh@gmail.com>
2024-05-18 16:11:02 -0400

    cmp fix and remove comments

--- lua/configs/cmp.lua
+++ lua/configs/cmp.lua
@@ -62,11 +62,10 @@ cmp.setup({
 })
 
 cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-            { name = 'cmp_git' }, },
-        {
-            { name = 'buffer' },
-        })
+    sources = {
+        { name = 'cmp_git' },
+        { name = 'buffer' },
+    }
 })
 
 cmp.setup.cmdline({ '/', '?' }, {
